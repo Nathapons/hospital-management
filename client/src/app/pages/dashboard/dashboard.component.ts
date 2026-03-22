505c9ff5bfae6b88e2173f63b014f39b2e26342d
Nuthapon <nuthaponm79@gmail.com>
2026-03-22 20:39:20 +0700

    feat: logout

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhrComponent } from './phr/phr.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -10,8 +13,13 @@ import { CreateUserComponent } from './create-user/create-user.component';
 })
 export class DashboardComponent {
   currentView = 'dashboard';
+  router = inject(Router);
 
   setView(view: string) {
     this.currentView = view;
+  }
+
+  async logout() {
+    this.router.navigate(['/login']);
   }
 }
